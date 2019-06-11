027a09ef6ab441928b031df7acd9c3c89f31ff43
yuba <ayuba@uwaterloo.ca>
2019-06-11 13:15:27 -0400

    tuesday 10. Need to check sigma(M) again I guess. Modified growth_factor

--- growth_factor.py
+++ growth_factor.py
@@ -4,36 +4,38 @@ from cosmo_parameters import *
 
 
 def growth_factor_pt(ol, om):
-    """approx furmula for growth factor given by press and turner"""
-    return (5*om/2)*1/(om**(4/7)-ol+(1+om/2)*(1+ol/70))
+    """approx formula for growth factor given by carroll press and turner(1992)"""
+    return (5*om/2)/(om**(4/7)-ol+(1+om/2)*(1+ol/70))
 
 
 def ibeta(x, a, b):
     return special.beta(a, b)*special.betainc(a, b, x)
 
-def growth_factor_z(z, N=50):
+def growth_factor_z(z, om0, ol0, ok, N=50):
     """Calculate the linear growth factor as a function of redshift"""
-    h = hubble_ratio(z, omega_l0, omega_m0, omega_r0)  #Calculating H(z)/H0
-    ol = omega_l(omega_l0, h)        #Calculating the Dark Energy density at z
-    om = omega_m(z, omega_m0, h)     #Matter density at z
+    h = hubble_ratio(z, ol0, om0, or0)  #Calculating H(z)/H0
+    ol = omega_l(ol0, h)        #Calculating the Dark Energy density at z
+    om = omega_m(z, om0, h)     #Matter density at z
     ok = 1-ol-om
-    n = np.arange(N)
-    tab = ((-1)**n*special.poch(1.5, n)/special.factorial(n))*(ok/(om**(2/3)*ol**(1/3)))**n*ibeta(ol/(om+ol),(5+2*n)/6,(2+2*n)/3)
-    res = 5*om**(1/3)/(6*ol**(5/6))*np.sum(tab)
-    return res
+    if ok == 0:
+        return (5*om/2)*1/(om**(4/7)-ol+(1+om/2)*(1+ol/70))
+    else:
+        n = np.arange(N)
+        tab = ((-1)**n*special.poch(1.5, n)/special.factorial(n))*(ok/(om**(2/3)*ol**(1/3)))**n*ibeta(ol/(om+ol),(5+2*n)/6,(2+2*n)/3)
+        res = 5*om**(1/3)/(6*ol**(5/6))*np.sum(tab)
+        return res
 
-def growth(om, ol, N=50):
-    """growth factor function in function of cosmo parameters only"""
-    ok = 1-om-ol
-    n = np.arange(N)
-    tab = ((-1)**n*special.poch(1.5, n)/special.factorial(n))*(ok/(om**(2/3)*ol**(1/3)))**n*ibeta(ol/(om+ol),(5+2*n)/6,(2+2*n)/3)
-    res = 5*om**(1/3)/(6*ol**(5/6))*np.sum(tab)
-    return res
+def growth(z, om0, ol0):
+    """Calculate the linear growth factor as a function of redshift"""
+    h = hubble_ratio(z, ol0, om0, 0)  #Calculating H(z)/H0
+    ol = omega_l(ol0, h)        #Calculating the Dark Energy density at z
+    om = omega_m(z, om0, h)     #Matter density at z
+    return (5*om/2)*1/(om**(4/7)-ol+(1+om/2)*(1+ol/70))
 
-def D(z):
+def D(z, om0, ol0):
     """Normalised linear growth factor"""
-    return growth_factor_z(z)/((1+z)*growth_factor_z(0))
+    return growth(z, om0, ol0)/((1+z)*growth(0, om0, ol0))
 
-def delta_c(z):
+def delta_c(z, om0, ol0):
     """critical overdensity"""
-    return 1.68/D(z)
+    return 1.68/D(z, om0, ol0)
--- halo_mass_function.py
+++ halo_mass_function.py
@@ -8,9 +8,9 @@ from fluctuation_rms import *
 
 
 
-def hmf(M, z=0, window='Gauss', sigma8=sigma8, H0=H_0, ombh2=ombh2, omch2=omch2, ns=ns):
-    del_c = delta_c(z)
-    sig = sigma_a_M(M, window=window, z=z, sigma8=sigma8, H0=H0, ombh2=ombh2, omch2=omch2, ns=ns, kmax=10)
+def hmf(M, z=0, window='Gauss', sigma8=sigma8, om0=omega_m0, ol0=omega_l0, H0=H_0, ombh2=ombh2, omch2=omch2, ns=ns):
+    del_c = delta_c(z, om0, ol0)
+    sig = sigma_a_M(M, window=window, z=0, sigma8=sigma8, H0=H0, ombh2=ombh2, omch2=omch2, ns=ns, kmax=10)
     dlsig = np.log(sig[1:]/sig[:-1])
     dlM = np.log(M[1:]/M[:-1])
     new_sig = (sig[1:]+sig[:-1])*0.5
@@ -24,13 +24,34 @@ def hmf(M, z=0, window='Gauss', sigma8=sigma8, H0=H_0, ombh2=ombh2, omch2=omch2,
 def fps(nu):
     return np.sqrt(2/np.pi)*nu*np.exp(-nu**2/2)
 
-
-M = np.logspace(11,16, 100)
-y1 = hmf(M)
-plt.loglog(M[1:], -y1)
+M = np.logspace(11,15, 100)
+z = [0,0.5,1,2]
+for el in z:
+    y1 = hmf(M, z=el, sigma8=0.8)
+    plt.loglog(M[1:], -y1, label='z='+str(el))
 plt.xlabel('M [$h^{-1}M_\odot$]', size = 15)
-plt.ylabel('n(M)', size = 15)
+plt.ylabel('n(M) [$h^4/Mpc^{3}/M_\odot$]', size = 15)
 plt.title('Press and Schechter halo mass function', size = 15)
+plt.legend()
+plt.show()
+
+sigma8 = [0.6,0.7,0.8,0.9,1,1.1]
+for el in sigma8:
+    y1 = hmf(M, z=0, sigma8=el)
+    plt.loglog(M[1:], -y1, label='$\sigma_8=$'+str(el))
+plt.xlabel('M [$h^{-1}M_\odot$]', size = 15)
+plt.ylabel('n(M) [$h^4/Mpc^{3}/M_\odot$]', size = 15)
+plt.title('Press and Schechter halo mass function', size = 15)
+plt.legend()
 plt.show()
 
 
+om1 = [0.1,0.2,0.3,0.4,0.5]
+for el in om1:
+    y1 = hmf(M, z=0.5, sigma8=0.8, om0=el)
+    plt.loglog(M[1:], -y1, label='$\Omega_m=$'+str(el))
+plt.xlabel('M [$h^{-1}M_\odot$]', size = 15)
+plt.ylabel('n(M) [$h^4/Mpc^{3}/M_\odot$]', size = 15)
+plt.title('Press and Schechter halo mass function', size = 15)
+plt.legend()
+plt.show()
